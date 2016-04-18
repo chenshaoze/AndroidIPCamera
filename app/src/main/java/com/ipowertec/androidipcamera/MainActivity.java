e53d01136f4631b3442f021e298227419a0e71f6
chenshaoze <chenshaoze2009@gmail.com>
2016-04-18 18:44:37 +0800

    add android rtsp server

--- app/src/main/java/com/ipowertec/androidipcamera/MainActivity.java
+++ app/src/main/java/com/ipowertec/androidipcamera/MainActivity.java
@@ -1,13 +1,53 @@
 package com.ipowertec.androidipcamera;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
+
+import net.majorkernelpanic.streaming.SessionBuilder;
+import net.majorkernelpanic.streaming.gl.SurfaceView;
+import net.majorkernelpanic.streaming.rtsp.RtspServer;
+
+import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static String TAG = "MainActivity";
+
+    private SurfaceView mSurfaceView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.activity_main);
+
+//        String[] queryParams = URI.create("rtsp://192.168.88.98:1234").getQuery().split("&");
+
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        mSurfaceView = (SurfaceView) findViewById(R.id.surface);
+
+        // Sets the port of the RTSP server to 1234
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(this).edit();
+        editor.putString(RtspServer.KEY_PORT, String.valueOf(1234));
+        editor.commit();
+
+        // Configures the SessionBuilder
+        SessionBuilder.getInstance()
+                .setSurfaceView(mSurfaceView)
+                .setPreviewOrientation(90)
+                .setContext(getApplicationContext())
+                .setAudioEncoder(SessionBuilder.AUDIO_AAC)
+                .setVideoEncoder(SessionBuilder.VIDEO_H264);
+
+        // Starts the RTSP server
+        this.startService(new Intent(this,RtspServer.class));
+
     }
 }
